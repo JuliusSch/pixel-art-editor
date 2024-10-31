4293ee3a4803103d5d6c469d7a108c5732c0cf9f
JuliusSch <35108709+JuliusSch@users.noreply.github.com>
2024-10-31 19:55:38 +0000

    Base project created

---
+++ src/Canvas.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Canvas = () => {
+  return (
+    <>
+      {/* <div style={{ width: '100%', height: '400px', border: '1px solid black' }}> */}
+      <div className="w-full h-96 border border-gray-400 bg-white">
+        Canvas Area
+      </div>;
+    </>
+  )
+};
+
+export default Canvas;
